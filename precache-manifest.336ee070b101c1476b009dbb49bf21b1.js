bdb340d14c318237d1706deff1be85e2f0a86626
sombra-ivan-arabchuk <ivan.arabchuk@sombrainc.com>
2020-01-04 16:35:38 +0000

    Updates

---
+++ precache-manifest.336ee070b101c1476b009dbb49bf21b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3a70320db04eeae5e1feec214a0849",
+    "url": "/burger-test-project/index.html"
+  },
+  {
+    "revision": "f5534f6696ee94924d6b",
+    "url": "/burger-test-project/static/css/main.486782e2.chunk.css"
+  },
+  {
+    "revision": "db1362db4a52fe4cc786",
+    "url": "/burger-test-project/static/js/2.ebd316bb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/burger-test-project/static/js/2.ebd316bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5534f6696ee94924d6b",
+    "url": "/burger-test-project/static/js/main.9fd8e2df.chunk.js"
+  },
+  {
+    "revision": "4a508f18e3b22c6ae533",
+    "url": "/burger-test-project/static/js/runtime-main.8eb63c28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burger-test-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
